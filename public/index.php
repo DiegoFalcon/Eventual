40f299d857c2a0726d2dbdfe7fc278f8ce909aae
DiegoFalcon <diego.lopez.falcon@gmail.com>
2015-09-17 19:27:02 -0700

    Primeros cambios

--- public/index.php
+++ public/index.php
@@ -1,23 +1,6 @@
+
 <?php
 
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylorotwell@gmail.com>
- */
-
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| our application. We just need to utilize it! We'll simply require it
-| into the script here so that we don't have to worry about manual
-| loading any of our classes later on. It feels nice to relax.
-|
-*/
 
 require __DIR__.'/../bootstrap/autoload.php';
 
@@ -49,7 +32,6 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-require("holamundo.php");
 
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
---
+++ resources/views/Principal.php
@@ -0,0 +1,11 @@
+
+
+
+<html>
+    <body>
+        <nav class="navbar navbar-inverse">
+	  ...
+</nav>
+
+    </body>
+</html
