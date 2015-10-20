89363edca360da7622ca63ef2b1ece33f9e29d42
DiegoFalcon <diego.lopez.falcon@gmail.com>
2015-10-19 18:55:06 -0700

    se agregaron los estilos

--- index.php
+++ index.php
@@ -7,8 +7,12 @@
     <meta name="description" content="">
     <meta name="Eventual" content="">
 
-    <title>Eventual</title>
+    <title>Eventual Web</title>
 
+  <link rel="icon"  type="image/png" href="eventualsimple.png" />
+
+
+<link rel="stylesheet" type="text/css" href="style.css">
     <!-- Bootstrap core CSS -->
     <!--<link href="dist/css/bootstrap.min.css" rel="stylesheet">-->
 <!-- Latest compiled and minified CSS -->
@@ -17,35 +21,12 @@
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
 
-<!-- Latest compiled and minified JavaScript -->
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-    <!-- Custom styles for this template -->
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<style type="text/css">
-</style>
-<script>
-	$(document).ready(function() {
-
-			$('#login').click(function(){
-				console.log("entra aqui");
-				window.location.href='login';
-			});
-	});//$(document).ready(function()
-	</script>
 
   </head>
 <!-- NAVBAR
 ================================================== -->
-  <body>
+  <body style="background:#ffaa00;">
 
 	<?php
       require_once("vistas/navBar.php");
@@ -53,31 +34,7 @@
 
 
 
-    <!-- Carousel
-    ================================================== -->
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-      <!-- Indicators -->
-      <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      </ol>
-      <div class="carousel-inner" role="listbox">
-        <div class="item active">
-          <div class="banner" align="center">
-            <img src="http://luzytextura.com/public/images/portfolio21bglogo-eventos-luzytextura.L01.jpg" alt="First slide" width="100%" height="500px" align="center">
-          </div>   
-
-          <div class="container">
-            <div class="carousel-caption">
-              <!--<h1>Play your favorite music.</h1>
-              <p>Play your favorite music has never been easier.</p>
-          -->
-            </div>
-          </div>
-        </div>
-        
-      </div>
-
-    </div><!-- /.carousel -->
+   <img src="eventual.png" class="centerLogo">
 
 
     
